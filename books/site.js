243df623f9412c691fd27a53a8d030db8e040560
JC Jamison <jam18002@byui.edu>
2025-12-01 12:29:39 -0800

    site building

---
+++ books/site.js
@@ -0,0 +1,12 @@
+const books = [
+    {
+        title: "Michael Vey: The Prisoner of Cell 25",
+        date: "July 10, 2012",
+        description: "Michael Vey, a teen with electric powers, teams up with her friends to rescue his mother from an evil corporation",
+        imgSrc: "Michael-Vey.jpg",
+        imgAlt: "Michael Vey in Cell 25",
+        ages: "10+",
+        genre: "Sci-fi, Adventure, Thriller",
+        stars: "⭐⭐⭐⭐⭐"
+    },
+]
